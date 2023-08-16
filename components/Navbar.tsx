eec9b3281164c3d75ea3aabf57bde90253869d6b
Heber Pereira <isaiahpereira88@gmail.com>
2023-08-15 20:33:38 -0700

    create startup sidebar menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,12 @@
-import { Menu } from "lucide-react"; // from chadcn-ui New York
 import React, { FC } from "react";
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+
+import MobileSidebar from "@/components/MobileSidebar";
 
 const Navbar: FC = () => {
   return (
     <div className="flex items-center p-4">
-      <Button variant="ghost" size="icon" className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
       </div>
