20697f7cc23c1a883f732fbcc511c687dc066ea1
Heber Pereira <isaiahpereira88@gmail.com>
2023-08-15 21:21:36 -0700

    fix heading issue in dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -57,9 +57,9 @@ const DashboardPage = () => {
   return (
     <div>
       <div className="mb-8 space-y-4">
-        <p className="text-2xl md:text-4xl font-bold text-center">
+        <h1 className="text-2xl md:text-4xl font-bold text-center">
           Explore the power of AI
-        </p>
+        </h1>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI - Experience the power of the future
         </p>
